7644f688f3aab48502aab69bac8e252eebb5bdec
juho <linre_@hotmail.com>
2021-08-13 10:08:02 +0300

    removed unneccesary comments

--- firstTutorial.py
+++ firstTutorial.py
@@ -16,19 +16,6 @@ class_names = ["t-shirt", "trouser", "pullover", "dress", "coat", "sandal", "shi
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-"""
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5, 5, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-
-plt.show()
-"""
-
 allreadyTrained = True
 
 if not allreadyTrained:
